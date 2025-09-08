b35c2495df815df4e85c81fa5a7731dadda9a7ae
kndsd <Kauenatanael000@gmail.com>
2025-09-08 14:10:36 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -12,30 +12,33 @@ print("""codigo\tfrutas\tpreço cheio\tdescoto
 k1= int (input("dijite produto"))
 k2= int(input("quilos:"))
 
-k4= 100
+
+
 
 match k1:
 
     case 1:
-        if k2 => 10
+        k6=("maça")
+        if k2 >= 10:
            k3= 2.20
+           
+
         else:
             k3= 2.50
 
     case 2:
-        if k2 => 10
+        k6=("morango")   
+        if k2 >= 10:
            k3= 1.50
 
         else: 
             k3 = 1.80
 
 
-k5= k3 * k2
 
-if k2 => 15
-    k3 = k3 * 0.1
-
-
-k5= k3 * k2  
+if k2 >= 15:
+    k4 = k3 * 0.1
+    k5 = k4 * k2
+    print(f"voce comprou {k2} kilos de {k6} preço total é {k5}")
 
   
